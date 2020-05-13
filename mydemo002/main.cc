751cb6ad5c4f625bb4ec7c143ffdcf7784152b8d
gongluck <1039994845@qq.com>
2020-05-13 11:58:25 +0800

    同一目录，多个源文件

---
+++ mydemo002/main.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 10:36:07 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 11:46:37
+ */
+
+#include "myprint.h"
+
+int main()
+{
+    myprint();
+    return 0;
+}
