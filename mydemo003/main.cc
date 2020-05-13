5b1dc26f3ac2fce3bb0aa08fc560b43fc614bec1
gongluck <1039994845@qq.com>
2020-05-13 15:36:44 +0800

    多个目录，多个源文件

---
+++ mydemo003/main.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 10:36:07 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 15:35:29
+ */
+
+#include "myprint/myprint.h"
+
+int main()
+{
+    myprint();
+    return 0;
+}
---
+++ mydemo003/myprint/myprint.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 11:45:19 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 15:13:31
+ */
+
+#include "myprint.h"
+#include <stdio.h>
+
+extern "C" void myprint()
+{
+    printf("mydemo003\n");
+}
