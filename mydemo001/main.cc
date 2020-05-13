b6270e097d276758cba015490ff1cb6d59871569
gongluck <1039994845@qq.com>
2020-05-13 10:46:44 +0800

    入门案例：单个源文件

---
+++ mydemo001/main.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 10:36:07 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 10:36:42
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("mydemo001\n");
+    return 0;
+}
