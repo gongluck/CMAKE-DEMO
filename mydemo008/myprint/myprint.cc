574299a492b5ddaf9c3952abe4a4089abd7ec0a2
gongluck <1039994845@qq.com>
2020-05-14 15:11:15 +0800

    构建CPack安装包

---
+++ mydemo008/myprint/myprint.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 11:45:19 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 15:43:29
+ */
+
+#include "myprint.h"
+#include <stdio.h>
+
+extern "C" void myprint()
+{
+    printf("mydemo008\n");
+}
