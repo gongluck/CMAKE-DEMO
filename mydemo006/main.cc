4fe8e24ec53fe219c17341f802aadcea60428de4
gongluck <1039994845@qq.com>
2020-05-14 10:32:11 +0800

    添加环境检查

---
+++ mydemo006/main.cc
@@ -0,0 +1,27 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 10:36:07 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 15:57:25
+ */
+
+#include "config.h"
+
+#ifdef USEMYPRINT
+#include "myprint/myprint.h"
+#else
+#include <stdio.h>
+    void myprint()
+    {
+        printf("mydemo006.\n");
+    }
+#endif
+
+int main()
+{
+    myprint();
+    #ifdef HAVE_PRINTF
+        printf("have printf\n");
+    #endif
+    return 0;
+}
