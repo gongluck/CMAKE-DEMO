6f8285949b941a3e199ffa901d1215f11e827150
gongluck <1039994845@qq.com>
2020-05-14 10:40:12 +0800

    添加版本号

---
+++ mydemo007/main.cc
@@ -0,0 +1,29 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-13 10:36:07 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-13 15:57:25
+ */
+
+#include <stdio.h>
+#include "config.h"
+
+#ifdef USEMYPRINT
+#include "myprint/myprint.h"
+#else
+#include <stdio.h>
+    void myprint()
+    {
+        printf("mydemo007.\n");
+    }
+#endif
+
+int main()
+{
+    printf("Version %d.%d\n", VERSION_MAJOR, VERSION_MINOR);
+    myprint();
+    #ifdef HAVE_PRINTF
+        printf("have printf\n");
+    #endif
+    return 0;
+}
