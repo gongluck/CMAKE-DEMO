cdb4d1ef654c8e467c87588f1db81ffdfac0b064
gongluck <1039994845@qq.com>
2020-05-15 21:25:51 +0800

    修复printf未定义错误

--- mydemo006/main.cc
+++ mydemo006/main.cc
@@ -5,12 +5,12 @@
  * @Last Modified time: 2020-05-13 15:57:25
  */
 
+#include <stdio.h>
 #include "config.h"
 
 #ifdef USEMYPRINT
 #include "myprint/myprint.h"
 #else
-#include <stdio.h>
     void myprint()
     {
         printf("mydemo006.\n");
