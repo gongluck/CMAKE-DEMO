27136bad78346bfc4b238fc60cf373c573a0ccb0
gongluck <1039994845@qq.com>
2020-05-17 18:23:47 +0800

    修复windows平台不生成lib的问题

--- mydemo/myfun/myfun.h
+++ mydemo/myfun/myfun.h
@@ -2,12 +2,18 @@
  * @Author: gongluck 
  * @Date: 2020-05-17 16:00:50 
  * @Last Modified by: gongluck
- * @Last Modified time: 2020-05-17 17:05:09
+ * @Last Modified time: 2020-05-17 18:21:40
  */
 
 #ifndef __MYFUN_H__
 #define __MYFUN_H__
 
-int myfun(int a, int b);
+#ifdef myfun_EXPORTS
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+DLL_EXPORT int myfun(int a, int b);
 
 #endif//__MYFUN_H__
