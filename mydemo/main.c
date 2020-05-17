334684a98cf8c670088ff50fd4ffa36b2d397a88
gongluck <1039994845@qq.com>
2020-05-17 17:49:01 +0800

    添加包含路径

--- mydemo/main.c
+++ mydemo/main.c
@@ -2,11 +2,11 @@
  * @Author: gongluck 
  * @Date: 2020-05-17 14:42:02 
  * @Last Modified by: gongluck
- * @Last Modified time: 2020-05-17 16:52:08
+ * @Last Modified time: 2020-05-17 17:46:14
  */
 
 #include <stdio.h>
-#include "myfun/myfun.h"
+#include "myfun.h"
 
 int main()
 {
