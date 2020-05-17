70466913604f9415ba3a4c9ee658fd93799ca4ff
gongluck <1039994845@qq.com>
2020-05-17 17:33:33 +0800

    博客文章代码：增加子模块

--- mydemo/main.c
+++ mydemo/main.c
@@ -1,14 +1,16 @@
 /*
  * @Author: gongluck 
  * @Date: 2020-05-17 14:42:02 
- * @Last Modified by:   gongluck 
- * @Last Modified time: 2020-05-17 14:42:02 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-17 16:52:08
  */
 
 #include <stdio.h>
+#include "myfun/myfun.h"
 
 int main()
 {
     printf("hello, cmake!\n");
+    printf("1+1=%d\n", myfun(1, 1));
     return 0;
 }
---
+++ mydemo/myfun/myfun.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: gongluck 
+ * @Date: 2020-05-17 16:00:50 
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-05-17 17:05:09
+ */
+
+#ifndef __MYFUN_H__
+#define __MYFUN_H__
+
+int myfun(int a, int b);
+
+#endif//__MYFUN_H__
