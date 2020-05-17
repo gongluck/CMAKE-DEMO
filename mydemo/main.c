c3435e58f6b3a02db8b0787a62d8a0e7b03cd8df
gongluck <1039994845@qq.com>
2020-05-18 03:23:08 +0800

    博客文章：自定义编译选项

--- mydemo/main.c
+++ mydemo/main.c
@@ -6,11 +6,16 @@
  */
 
 #include <stdio.h>
+
+#ifdef USESUBMODULE
 #include "myfun.h"
+#endif//USESUBMODULE
 
 int main()
 {
     printf("hello, cmake!\n");
+#ifdef USESUBMODULE
     printf("1+1=%d\n", myfun(1, 1));
+#endif//USESUBMODULE
     return 0;
 }
